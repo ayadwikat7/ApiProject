2b9d700a87ad5e6aedbd3bd9f183728c3f7c848a
unknown <ayadwikat772@gmail.com>
2025-11-19 08:13:03 +0200

    L1

---
+++ DAL/Data/ApplicationDpContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class ApplicationDpContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDpContext(DbContextOptions<ApplicationDpContext> options)
+            : base(options)
+        {
+        }
+
+        // Example DbSet:
+        // public DbSet<Category> Categories { get; set; }
+    }
+}
+
---
+++ KASHPE.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KASHPE.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+
+
+    {
+
+        private readonly ApplicationDpContext _Context;
+        public CategoriesController(ApplicationDpContext context)
+        {
+            _Context = context;
+        }
+
+    }
+}
